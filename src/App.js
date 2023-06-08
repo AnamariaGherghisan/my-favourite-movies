c127e8ad04be16c09c4cc22184c3f993504605da
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2023-06-08 01:12:33 +0100

    complete style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Banner } from "./components/Banner";
 import { MovieCard } from "./components/MovieCard";
 import { Movies } from "./components/Movies";
+//import { FeaturedMovie } from "./components/FeaturedMovie";
+//import { FeaturedMovies } from "./components/FeaturedMovies";
+//import { FeaturedMoviesCarousel } from "./components/FeaturedMoviesCarousel";
+//import { FeaturedMoviesSlider } from "./components/FeaturedMoviesSlider";
+//import { FeaturedMoviesSlider2 } from "./components/FeaturedMoviesSlider2";
+//import { FeaturedMoviesSlider3 } from "./components/FeaturedMoviesSlider3";
+import "./index.css";
+
 const movies = [
-  {imageUrl:"https://images.unsplash.com/photo-1685682160927-3597668ec05c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1870&q=80", title:"Movie 1",},
-  {imageUrl:"https://images.unsplash.com/photo-1685570788011-9a136a8582fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", title:"Movie 2",},
-  {imageUrl:"https://images.unsplash.com/photo-1682685796852-aa311b46f50d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", title:"Movie 3",},];
+  {imageUrl:" https://www.themoviedb.org/t/p/w300_and_h450_bestv2/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg", title:"Movie 1",},
+  {imageUrl:"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/pqiRuETmuSybfnVZ7qyeoXhQyN1.jpg", title:"Movie 2",},
+  {imageUrl:"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/rejrD9ovTHJbfmpLM0mbEliEPV6.jpg", title:"Movie 3",},];
 export const App = () => {
   return <div>
     <Banner></Banner>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,25 @@
+import React, { useState } from 'react';
+import { RiAddLine, RiSubtractLine } from 'react-icons/ri';
 
 export const MovieCard = ({ imageUrl, title }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleAddToFavorites = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <div className="card movie-card m-3">
-      <img src={imageUrl} className="card-img-top"  alt={title} />
+      <div className="movie-card-overlay">
+        <button type="button" className="btn btn-primary" onClick={handleAddToFavorites}>
+          {isFavorite ? <RiSubtractLine /> : <RiAddLine />}
+          {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+        </button>
+      </div>
+      <img src={imageUrl} className="card-img-top" alt={title} />
       <div className="card-body text-center">
         <h5 className="card-title">{title}</h5>
-        <button type="button" className="btn btn-primary">Add to fav</button>
       </div>
-    </div>  
+    </div>
   );
 };
